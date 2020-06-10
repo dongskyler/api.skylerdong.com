98117de2cc2ca913952460741efec93613d63713
Skyler Dong <dongskyler@gmail.com>
2020-06-10 18:50:37 -0500

    Infrastructure: add graphql, but more to be done (#9)
    * Intermediate stage
    
    * Add graphql, but more to be done

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const createError = require('http-errors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var imagesRouter = require('./routes/images');
-var blogsRouter = require('./routes/blogs');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const imagesRouter = require('./routes/images');
+const blogsRouter = require('./routes/blogs');
+
+const app = express(); // create express server
+
+const blogs = [];
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,19 +26,74 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json()); // use body-parser middleware to parse incoming json
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/images', imagesRouter);
 app.use('/blogs', blogsRouter);
 
+app.use('/graphql', graphqlHttp({ // set up our graphql endpoint with the express-graphql middleware
+  // build a graphql schema
+  schema: buildSchema(`
+        type Blog {
+            _id: ID!
+            title: String!
+            text: String!
+            description: String!
+            date: String
+        }
+
+        input BlogInput {
+            title: String!
+            text: String!
+            description: String!
+            date: String
+        }
+
+
+        type blogQuery {
+            blogs: [Blog!]!
+        }
+
+        type blogMutation {
+            createBlog(blogInput: BlogInput): Blog
+        }
+
+        schema {
+            query: blogQuery
+            mutation: blogMutation
+        }
+    `),
+  rootValue: {
+    blogs: () => {
+      return blogs; // return blogs instead of hardcoded array
+    },
+    createBlog: (args) => {
+      // const blogText = args.text
+      // return blogText
+      const blog = { // create new blog object
+        _id: "123456", // this is hardcoded temporarily
+        title: args.blogInput.title,
+        text: args.blogInput.text,
+        description: args.blogInput.description,
+        date: new Date().toISOString
+      }
+
+      blogs.push(blog); // push new blog object onto array
+      return blog;
+    }
+  }, // an object with resolver functions
+  graphiql: true // enable the graphiql interface to test our queries
+}))
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
