1d0f7b40ef4d550034f6b735f1be31e53546dc80
Skyler Dong <dongskyler@gmail.com>
2020-06-15 08:22:51 -0500

    Add GraphQL schema stitching

---
+++ server/resolvers/message.js
@@ -0,0 +1,65 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const resolvers = {
+  Query: {
+    message: (parent, { id }, { models }) => {
+      return messages[id];
+    },
+    messages: (parent, { id }, { models }) => {
+      return Object.values(messages);
+    },
+  },
+
+  Mutation: {
+    createMessage: (parent, { text }, { me, models }) => {
+      const id = uuidv4();
+      const message = {
+        id,
+        text,
+        userId: me.id,
+      };
+
+      messages[id] = message;
+      users[me.id].messageIds.push(id);
+
+      return message;
+    },
+    updateMessage: (parent, { id, text }, { me, models }) => {
+      if (!id || !text) {
+        return false;
+      }
+
+      const message = {
+        id,
+        text,
+        userId: me.id,
+      };
+
+      messages[id] = message;
+
+      return true;
+    },
+    deleteMessage: (parent, { id }, { models }) => {
+      const { [id]: message, ...otherMessages } = messages;
+
+      if (!message) {
+        return false;
+      }
+
+      messages = otherMessages;
+
+      return true;
+    },
+  },
+
+  Message: {
+    // user: (parent, args, { me }) => {
+    //   return me;
+    // },
+    user: (message, args, { models }) => {
+      return users[message.userId];
+    },
+  },
+};
+
+export default resolvers;
---
+++ server/resolvers/user.js
@@ -0,0 +1,23 @@
+const resolvers = {
+  Query: {
+    me: (parent, args, { me }) => {
+      return me;
+    },
+    user: (parent, { id }, { models }) => {
+      return users[id];
+    },
+    users: (parent, args, { models }) => {
+      return Object.values(users);
+    },
+  },
+
+  User: {
+    messages: (user, args, { models }) => {
+      return Object.values(messages).filter(
+        (message) => message.userId === user.id
+      );
+    },
+  },
+};
+
+export default resolvers;
