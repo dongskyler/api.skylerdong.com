eaacbe3731780118563f793435eea59b3d52fdb5
Skyler Dong <dongskyler@gmail.com>
2020-07-06 08:09:06 -0500

    Add resume route & resume draft (#47)
    * Add resume route & resume draft
    
    * Remove basics route

---
+++ server/resolvers/user-legacy.js
@@ -0,0 +1,33 @@
+/**
+ * @summary Back-end API for my personal websites.
+ * @author Skyler Dong <skyler@skylerdong.com>
+ * @copyright 2020 Skyler Dong
+ * @version 0.0.0
+ *
+ * Back-end API for my websites, built with Node, Express,
+ * Apollo Server, GraphQL, MongoDB Atlas and ElasticSearch
+ *
+ */
+const resolvers = {
+  Query: {
+    me: (parent, args, { me }) => {
+      return me;
+    },
+    user: (parent, { id }, { models }) => {
+      return users[id];
+    },
+    users: (parent, args, { models }) => {
+      return Object.values(users);
+    },
+  },
+
+  User: {
+    messages: (user, args, { models }) => {
+      return Object.values(messages).filter(
+        (message) => message.userId === user.id
+      );
+    },
+  },
+};
+
+export default resolvers;
