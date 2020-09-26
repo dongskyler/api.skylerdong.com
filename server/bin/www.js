783154f11d9f4569d0e6096cce9a1dbeecedc5b5
Skyler Dong <dongskyler@gmail.com>
2020-09-26 11:56:31 -0500

    :wrench: Update config files & cleanup code (#145)

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,15 +1,5 @@
 #!/usr/bin/env node
 
-/**
- * @summary Back-end API for my personal websites.
- * @author Skyler Dong <skyler@skylerdong.com>
- * @copyright 2020 Skyler Dong
- * @version 0.0.0
- *
- * Back-end API for my websites, built with Node, Express,
- * Apollo Server, GraphQL, MongoDB Atlas and ElasticSearch
- *
- */
 import dotenv from 'dotenv';
 import http from 'http';
 import mongoose from 'mongoose';
