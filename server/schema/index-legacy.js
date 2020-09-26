9186a3e32df47d712ded5404afbc1e677cfd0763
Skyler Dong <dongskyler@gmail.com>
2020-09-26 11:52:52 -0500

    :wrench: Update config files & cleanup code

--- server/schema/index-legacy.js
+++ server/schema/index-legacy.js
@@ -1,13 +1,3 @@
-/**
- * @summary Back-end API for my personal websites.
- * @author Skyler Dong <skyler@skylerdong.com>
- * @copyright 2020 Skyler Dong
- * @version 0.0.0
- *
- * Back-end API for my websites, built with Node, Express,
- * Apollo Server, GraphQL, MongoDB Atlas and ElasticSearch
- *
- */
 import { gql } from 'apollo-server-express';
 
 import userSchema from './user';
